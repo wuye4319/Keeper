901e439a8da3aa1f6d2291983c14e10062ec0bef
Nero home <14380411@qq.com>
2018-11-28 01:39:49 +0800

    clear

--- BuilderPlugin/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-Change History
-==============
-
-v1.0.0
----
-* Create File
-插件依赖：
-keeper-core     核心包，依赖了color包
--- BuilderPlugin/admin/base/rout.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by nero on 2017/12/26.
- */
-
-require('../router/builder')
-require('../router/product')
